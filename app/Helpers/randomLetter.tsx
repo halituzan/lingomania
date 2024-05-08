36321fe5b2a40a45b10d85b40bf4ba6b0465f3d9
halit uzan <halit.uzan@gmail.com>
2024-05-09 01:31:52 +0300

    delete ğ letter

--- app/Helpers/randomLetter.tsx
+++ app/Helpers/randomLetter.tsx
@@ -1,12 +1,41 @@
 const randomLetter = () => {
-    // Türkçe harfleri içeren bir dizi oluştur
-    var turkceHarfler = ['a', 'b', 'c', 'ç', 'd', 'e', 'f', 'g', 'ğ', 'h', 'ı', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'ö', 'p', 'r', 's', 'ş', 't', 'u', 'ü', 'v', 'y', 'z'];
+  // Türkçe harfleri içeren bir dizi oluştur
+  var turkceHarfler = [
+    "a",
+    "b",
+    "c",
+    "ç",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "ı",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "ö",
+    "p",
+    "r",
+    "s",
+    "ş",
+    "t",
+    "u",
+    "ü",
+    "v",
+    "y",
+    "z",
+  ];
 
-    // Rastgele bir indeks seç
-    var rastgeleIndex = Math.floor(Math.random() * turkceHarfler.length);
+  // Rastgele bir indeks seç
+  var rastgeleIndex = Math.floor(Math.random() * turkceHarfler.length);
 
-    // Seçilen indeksteki harfi döndür
-    return turkceHarfler[rastgeleIndex];
-}
+  // Seçilen indeksteki harfi döndür
+  return turkceHarfler[rastgeleIndex];
+};
 
-export default randomLetter
+export default randomLetter;
