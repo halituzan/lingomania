c588a0fa6e49e8de81dd17ebb1bde788d8d1f610
halit uzan <halit.uzan@gmail.com>
2024-05-14 21:39:27 +0300

    updates

--- PageApi/db/connection.tsx
+++ PageApi/db/connection.tsx
@@ -1,8 +1,13 @@
 import mongoose from "mongoose";
 
 const connectDBV2 = async () => {
+  if (!process.env.NEXT_PUBLIC_MONGODB_URI) {
+    throw new Error(
+      "Environment variable NEXT_PUBLIC_MONGODB_URI is not defined."
+    );
+  }
   try {
-    await mongoose.connect(process.env.NEXT_PUBLIC_MONGODB_URI || "");
+    await mongoose.connect(process.env.NEXT_PUBLIC_MONGODB_URI);
     console.log("MongoDB Version 2 connected");
   } catch (error) {
     console.error(error);
